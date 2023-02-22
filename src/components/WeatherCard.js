4080ea683d2d70dc9b0db14c9be515f1e9db8955
TaureHorn <abaird0893@gmail.com>
2023-02-22 16:55:57 +0000

    AB: Tuesday | logic for API working

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,4 +1,6 @@
 import Card from 'react-bootstrap/Card';
+import arrow from '../resources/arrow.svg';
+// import arrow from './resources/arrow.svg';
 
 function WeatherCard(props) {
   const weather = props.weather;
@@ -11,13 +13,21 @@ function WeatherCard(props) {
             <h1>{weather.date}</h1>
           </Card.Title>
           <Card.Text>
-            <div id="icon">{weather.image}</div>
+            <img id="icon" src={weather.image} />
             <div id="description">{weather.description}</div>
             <div>
               <div id="minTemp">{weather.minTemp}</div>
               <div id="maxTemp">{weather.maxTemp}</div>
             </div>
             <div id="windSpeed">{weather.windSpeed}</div>
+            <img src = { arrowIcon } 
+            // add wind direction arrow icon here AFTER it has been rotated
+            // style={[
+            //   {
+            //     transform: [{ rotate: `${weather.windDirection}deg` }],
+            //   }, ]} 
+              />
+            <div id="windDirection">{weather.windDirection}</div>
           </Card.Text>
         </Card.Body>
       </div>
